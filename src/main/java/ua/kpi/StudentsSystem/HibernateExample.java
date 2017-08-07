da498135045dcebaf6e6f08cc05df0f85b88a914
jekafortuna <evgeniy.fortuna@gmail.com>
2017-08-07 22:14:00 +0300

    Simple Students system

---
+++ src/main/java/ua/kpi/StudentsSystem/HibernateExample.java
@@ -0,0 +1,36 @@
+package ua.kpi.StudentsSystem;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import ua.kpi.StudentsSystem.entity.Student;
+
+/**
+ * Created by Evgeniy on 31.07.2017.
+ */
+public class HibernateExample {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        /*
+        Student student = new Student();
+        student.setFirstName("John");
+        student.setLastName("Terry");
+        student.setAge(30);
+
+        session.save(student); */
+
+        Student student = session.get(Student.class, 2);
+        System.out.println(student);
+//        student.setAge(38);
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
+    }
+}
